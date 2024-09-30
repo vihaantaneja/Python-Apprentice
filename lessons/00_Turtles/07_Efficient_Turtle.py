9fc5d8767c21018be730e5dbd68d117e8cbd2da3
vihaantaneja <vihaantaneja@icloud.com>
2024-09-30 23:57:20 +0000

    done

--- lessons/00_Turtles/07_Efficient_Turtle.py
+++ lessons/00_Turtles/07_Efficient_Turtle.py
@@ -1,5 +1,4 @@
-
-""""
+"""
 More Efficient Turtles
 
 Use what you've learned about functions and variables to make a program that
@@ -15,24 +14,12 @@ tina = turtle.Turtle()                  # Create a turtle named tina
 tina.shape('turtle')                    # Set the shape of the turtle to a turtle
 tina.speed(0)                           # Make the turtle move as fast, but not too fast. 
 
-def draw_polygon(sides):
-
-    angle = ... # Calculate angle from number of sides
-    
-    for i in range(...):                 # Loop through the number of sides
-        ...                              # Move tina forward by the forward distance
-        ...                              # Turn tina left by the left turn
-
-
-draw_polygon(...)                        # Draw a square
-
-...                                      # Move tina to another spot on the screen
-
-draw_polygon(...)                        # Draw a pentagon
-
-...                                      # Move tina to another spot on the screen
-
-draw_polygon(...)                        # Draw a hexagon
+def draw_polygon(sides):    
+    angle = 360/sides        
+    tina.fd(100)
+    tina.lt(angle)
+for i in range():               
+    draw_polygon(123)                        # Draw a hexagon
 
 
-turtle.exitonclick()                     # Close the window when we click on it
+turtle.done                    # Close the window when we click on it
